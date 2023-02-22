d531897b26dbb9033e3873f63e5eee4a07e4dba6
PlayerJoker <fabioaugusto.almeida2@gmail.com>
2023-02-22 15:02:34 -0300

    Ajustando report git actions

--- src/test/java/br/projeto/automacao/web/runners/RunnerTest.java
+++ src/test/java/br/projeto/automacao/web/runners/RunnerTest.java
@@ -9,7 +9,7 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.UNDERSCORE;
 		features = "src/test/resources/features/compra_de_menos_tres_produtos.feature",
 		glue = "br.projeto.automacao.steps",
 		tags = "@realizarOCadastroDoCliente",
-		//plugin = {"pretty", "html:target/report-html", "json:target/report.json"},
+		plugin = {"pretty", "html:target/report.html", "json:target/report.json"},
 		monochrome = true,
 		snippets = UNDERSCORE,
 		dryRun = false
